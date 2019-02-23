12a1ab70a2d3f357969140aff96913e54443996a
Andrew Carpenter <andrew@audioinhd.com>
2019-02-23 00:39:28 -0500

    Use mvt instead of ava for tests

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
-// setup
-var intvls = {s: 1000, m: 60000, h: 3600000, d: 86400000}
+var intvls = { s: 1000, m: 60000, h: 3600000, d: 86400000 }
 module.exports = function (str) {
   var num = parseFloat(str)
   var intvl = (str + '').replace((num + ''), '').trim().charAt(0)
--- test.js
+++ test.js
@@ -1,36 +1,46 @@
 'use strict'
 
-// setup
-import test from 'ava'
-import picoMs from './index'
+const { runTests, start, finish, test } = require('mvt')
+const picoMs = require('./index')
 const ms = 3600000
 
-test('seconds string parses as expected', (assert) => {
-  assert.is(ms, picoMs('3600s'))
-  assert.is(ms, picoMs('3600 sec'))
-  assert.is(ms, picoMs('3600 seconds'))
-})
+runTests(async () => {
+  start(`Testing my pico-ms`)
 
-test('minutes string parses as expected', (assert) => {
-  assert.is(ms, picoMs('60m'))
-  assert.is(ms, picoMs('60 mins'))
-  assert.is(ms, picoMs('60 minutes'))
-})
+  test('seconds string parses as expected', () => {
+    test('3600s', ms, picoMs('3600s'))
+    test('3600 sec', ms, picoMs('3600 sec'))
+    test('3600 seconds', ms, picoMs('3600 seconds'))
+    return true
+  })
 
-test('hours string parses as expected', (assert) => {
-  assert.is(ms, picoMs('1h'))
-  assert.is(ms, picoMs('1hr'))
-  assert.is(ms, picoMs('1 hour'))
-})
+  test('minutes string parses as expected', () => {
+    test('60m', ms, picoMs('60m'))
+    test('60 mins', ms, picoMs('60 mins'))
+    test('60 minutes', ms, picoMs('60 minutes'))
+    return true
+  })
 
-test('days string parses as expected', (assert) => {
-  assert.is(ms, picoMs(`${1 / 24} days`))
-  let daysMs = 432000000
-  assert.is(daysMs, picoMs('5d'))
-  assert.is(daysMs, picoMs('5 days'))
-})
+  test('hours string parses as expected', () => {
+    test('1h', ms, picoMs('1h'))
+    test('1hr', ms, picoMs('1hr'))
+    test('1 hour', ms, picoMs('1 hour'))
+    return true
+  })
 
-test('raw string or number returns as ms number', (assert) => {
-  assert.is(ms, picoMs(3600000))
-  assert.is(ms, picoMs('3600000'))
+  test('days string parses as expected', () => {
+    const daysMs = 432000000
+    test(`${1 / 24} days`, ms, picoMs(`${1 / 24} days`))
+    test('5d', daysMs, picoMs('5d'))
+    test('5 days', daysMs, picoMs('5 days'))
+    return true
+  })
+
+  test('raw string or number returns as ms number', (assert) => {
+    test(`3600000 as integer`, ms, picoMs(3600000))
+    test(`3600000`, ms, picoMs('3600000'))
+    return true
+  })
+
+  finish()
 })

fa75f1a49552ea85f357cfc5f8316f8c1a5eaffe
Andrew Carpenter <andrew@audioinhd.com>
2023-01-18 19:53:10 -0500

    No need for replace here

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var intvls = { s: 1000, m: 60000, h: 3600000, d: 86400000 }
 module.exports = function (str) {
-  var num = parseFloat(str)
-  var intvl = (str + '').replace((num + ''), '').trim().charAt(0)
+  var num = parseFloat(str || '')
+  var intvl = (str + '').charAt(num.toString().length)
   return num * (intvls[intvl] || 1)
 }

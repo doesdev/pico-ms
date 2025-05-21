ddc3b7683f200a66246653dd2a7d1901de0de436
Andrew Carpenter <andrew@audioinhd.com>
2025-05-21 13:39:09 -0400

    Update to ESM, more robust logic

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-'use strict'
+const intvls = { s: 1000, m: 60000, h: 3600000, d: 86400000 }
 
-var intvls = { s: 1000, m: 60000, h: 3600000, d: 86400000 }
-module.exports = function (str) {
-  var num = parseFloat(str || '')
-  var intvl = (str + '').charAt(num.toString().length)
+export default (str) => {
+  if (!str) return 0
+
+  const strType = typeof str
+
+  if (strType === 'number') return str
+  if (strType !== 'string') return null
+
+  const num = parseFloat(str)
+  const intvl = str.slice(num.toString().length).trim()[0]
+
+  if (intvl && !intvls[intvl]) return null
+
   return num * (intvls[intvl] || 1)
 }
--- test.js
+++ test.js
@@ -1,46 +1,34 @@
-'use strict'
+import test from 'mvt'
+import picoMs from './index.js'
 
-const { runTests, start, finish, test } = require('mvt')
-const picoMs = require('./index')
 const ms = 3600000
 
-runTests(async () => {
-  start(`Testing my pico-ms`)
+test('seconds string parses as expected', (assert) => {
+  assert.is(picoMs('3600s'), ms)
+  assert.is(picoMs('3600 sec'), ms)
+  assert.is(picoMs('3600 seconds'), ms)
+})
 
-  test('seconds string parses as expected', () => {
-    test('3600s', ms, picoMs('3600s'))
-    test('3600 sec', ms, picoMs('3600 sec'))
-    test('3600 seconds', ms, picoMs('3600 seconds'))
-    return true
-  })
+test('minutes string parses as expected', (assert) => {
+  assert.is(picoMs('60m'), ms)
+  assert.is(picoMs('60 mins'), ms)
+  assert.is(picoMs('60 minutes'), ms)
+})
 
-  test('minutes string parses as expected', () => {
-    test('60m', ms, picoMs('60m'))
-    test('60 mins', ms, picoMs('60 mins'))
-    test('60 minutes', ms, picoMs('60 minutes'))
-    return true
-  })
+test('hours string parses as expected', (assert) => {
+  assert.is(picoMs('1h'), ms)
+  assert.is(picoMs('1hr'), ms)
+  assert.is(picoMs('1 hour'), ms)
+})
 
-  test('hours string parses as expected', () => {
-    test('1h', ms, picoMs('1h'))
-    test('1hr', ms, picoMs('1hr'))
-    test('1 hour', ms, picoMs('1 hour'))
-    return true
-  })
+test('days string parses as expected', (assert) => {
+  const daysMs = 432000000
+  assert.is(picoMs(`${1 / 24} days`), ms)
+  assert.is(picoMs('5d'), daysMs)
+  assert.is(picoMs('5 days'), daysMs)
+})
 
-  test('days string parses as expected', () => {
-    const daysMs = 432000000
-    test(`${1 / 24} days`, ms, picoMs(`${1 / 24} days`))
-    test('5d', daysMs, picoMs('5d'))
-    test('5 days', daysMs, picoMs('5 days'))
-    return true
-  })
-
-  test('raw string or number returns as ms number', (assert) => {
-    test(`3600000 as integer`, ms, picoMs(3600000))
-    test(`3600000`, ms, picoMs('3600000'))
-    return true
-  })
-
-  finish()
+test('raw string or number returns as ms number', (assert) => {
+  assert.is(picoMs(3600000), ms)
+  assert.is(picoMs('3600000'), ms)
 })
